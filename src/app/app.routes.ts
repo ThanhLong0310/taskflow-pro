bb1c5271b053596bfe9ea73431e99bfc354896e9
ThanhLong0310 <long.lt0310@gmail.com>
2026-03-16 07:17:30 +0700

    feat: xây dựng giao diện trang Dashboard và cấu hình routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,15 @@ export const routes: Routes = [
   // Tự động chuyển hướng về trang login khi truy cập trang chủ
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   
-  // Khai báo đường dẫn cho trang login
+  // Đường dẫn trang Đăng nhập
   {
     path: 'login',
     loadComponent: () => import('./features/auth/login/login').then(m => m.LoginComponent)
+  }, 
+  // Đường dẫn trang Dashboard
+  {
+    path: 'dashboard',
+    // Sửa m.DashboardComponent thành m.Dashboard
+    loadComponent: () => import('./features/dashboard/dashboard').then(m => m.Dashboard)
   }
-];  
+];
